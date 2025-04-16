9b2ad37a6edd35a026fda563b80a3f877ce7d9ca
Yash Dixit <yashdixit0885@gmail.com>
2025-04-16 13:51:29 -0600

    🧵 Fix threading error by limiting threads on Render
    Former-commit-id: 451881f8b4ab02be32a57314b453551975aa8a35

--- app/main.py
+++ app/main.py
@@ -1,3 +1,10 @@
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import analyze
