875f05890fd8e5dd5489187d2a49d3c94c7e05d6
Yash Dixit <yashdixit0885@gmail.com>
2025-04-16 13:38:17 -0600

    added frontend env variables
    Former-commit-id: 0c2143b026d8d7caabd4c42c565498ae75a5f601

--- investment-analyst-frontend/src/api/fetchAnalysis.js
+++ investment-analyst-frontend/src/api/fetchAnalysis.js
@@ -1,5 +1,8 @@
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
 export async function fetchAnalysis(ticker) {
-    const response = await fetch(`http://localhost:8000/analyze/?ticker=${ticker}&generateReport=true`);
-    if (!response.ok) throw new Error("Failed to fetch analysis");
-    return response.json();
+  const url = `${baseUrl}/analyze?ticker=${ticker}&generateReport=true`;
+  const res = await fetch(url);
+  if (!res.ok) throw new Error("Failed to fetch analysis");
+  return res.json();
 }
